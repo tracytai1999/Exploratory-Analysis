cbafc816a30e80fd28671e07a16b6d016be231fc
lquan02 <76966572+lquan02@users.noreply.github.com>
2021-02-19 17:09:58 -0800

    Add files via upload

---
+++ table.R
@@ -0,0 +1,21 @@
+#run library 
+library(ggplot2)
+library(tidyverse)
+library(lintr)
+library(dplyr)
+
+#run csv file 
+gdp_data <- read_csv("GDP.csv")
+population_data <- read_csv("population.csv")
+
+#create new data frame
+new_gdp_df <- gdp_data %>%
+  select(State, stateGDP)
+
+new_pop_df <- population_data %>%
+  select(State, Pop, density)
+
+#joining data frame
+combine_df <- new_gdp_df %>%
+  left_join(new_pop_df, by = "State") %>%
+  group_by(State) 
