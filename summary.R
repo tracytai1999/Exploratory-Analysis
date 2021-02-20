1e819e913b5a3887a6e0cc5502a01c9ac657e4c2
邰子扬 <taiziyang@s-MacBook-Air.local>
2021-02-19 17:12:57 -0800

    Update files

--- summary.R
+++ summary.R
@@ -1,21 +1,5 @@
 # Summary information
 
-#load packages
-covid19 <- read.csv("COVID19_state.csv")
-population <- read.csv("population.csv")
-
-#state with highest covid-19 infected population
-
-highest_state <- covid19 %>%
-  filter(Infected == max(Infected, na.rm = T)) %>%
-  pull(State)
-
-# state with highest population
-highest_pop_state <- population %>%
-  filter(Pop == max(Pop, na.rm = T)) %>%
-  pull(State)
-
-# 
 
 
 
